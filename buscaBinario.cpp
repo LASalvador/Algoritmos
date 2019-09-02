76b61ec9e0570ac9e1d366682dab46608d370a95
Lucas Salvador <lucas.salvador11358@gmail.com>
2019-09-02 04:58:50 -0300

    finalizando busca binaria

--- buscaBinario.cpp
+++ buscaBinario.cpp
@@ -5,23 +5,23 @@ typedef long long ll;
 
 int primos[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}; 
 
-int binarySearch(int target)
-{
+int binarySearch(int array[], int tamanho,int target)
+{	
 	int min = 0;
-	int max = sizeof(primos);
+	int max = tamanho - 1;
 	int guess;
 	while(min <= max)
 	{
 		guess = (min + max)/2;
-		if(primos[guess] == target)
+		if(array[guess] == target)
 		{
 			return guess;
 		}
-		else if(primos[guess] < target){
-			min = guess - 1;
+		else if(array[guess] < target){
+			min = guess + 1;
 		}
 		else {
-			max = guess + 1;
+			max = guess - 1;
 		}
 	}
 	return -1;
@@ -29,8 +29,10 @@ int binarySearch(int target)
 
 int main(int argc, char const *argv[])
 {	
-	cout << "comecei";
-	int resultado = binarySearch(73);
+	int numero;
+	cin >> numero;
+
+	int resultado = binarySearch(primos,24,numero);
 
 	cout << "Found prime at index: " << resultado << "\n";
 
